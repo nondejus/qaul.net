7779eed3982eef0d69dfb56bb122a137d15b8a5e
jess 3jane <me@jess.coffee>
2020-01-06 21:00:24 +0100

    http-api-binary: fix user scope

--- clients/http-test/src/main.rs
+++ clients/http-test/src/main.rs
@@ -1,13 +1,12 @@
 use libqaul::*;
 use libqaul_http::*;
-use std::sync::Arc;
 use ratman::Router;
 
 fn main() {
     let r = Router::new();
 
-    let qaul = Arc::new(Qaul::new(r));
-    qaul.user_create("acab").expect("Failed to create test user!");
+    let qaul = Qaul::new(r);
+    qaul.users().user_create("acab").expect("Failed to create test user!");
     
     let _server = ServerBuilder::new(qaul.clone()).start("0.0.0.0:9090")
         .expect("Failed to start qaul.net API server (0.0.0.0:9090)!");
