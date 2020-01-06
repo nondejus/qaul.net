fa022ac24dd35d1c423633ff72365b1d06e2f0fc
jess 3jane <me@jess.coffee>
2020-01-06 21:06:36 +0100

    http-api-binary: user correct user creation function

--- clients/http-test/src/main.rs
+++ clients/http-test/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let r = Router::new();
 
     let qaul = Qaul::new(r);
-    qaul.users().user_create("acab").expect("Failed to create test user!");
+    qaul.users().create("acab").expect("Failed to create test user!");
     
     let _server = ServerBuilder::new(qaul.clone()).start("0.0.0.0:9090")
         .expect("Failed to start qaul.net API server (0.0.0.0:9090)!");
