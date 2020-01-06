b53a549f5cf6cc07e18e0b88be3792ef5dd5bf41
jess 3jane <me@jess.coffee>
2020-01-06 20:30:39 +0100

    http-test-binary: use ServerBuilder for http-api

--- clients/http-test/src/main.rs
+++ clients/http-test/src/main.rs
@@ -1,11 +1,12 @@
 use libqaul::*;
 use libqaul_http::*;
+use std::sync::Arc;
 
 fn main() {
-    let qaul = Qaul::start();
+    let qaul = Arc::new(Qaul::start());
     qaul.user_create("acab").expect("Failed to create test user!");
     
-    let _server = ApiServer::new(&qaul, "0.0.0.0:9090")
+    let _server = ServerBuilder::new(qaul.clone()).start("0.0.0.0:9090")
         .expect("Failed to start qaul.net API server (0.0.0.0:9090)!");
 
     #[allow(deprecated)]
